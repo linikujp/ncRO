ba71cadcf009fdd3a7dc6e735655dce27cab497f
linikujp <linikujp@gmail.com>
2015-05-16 23:42:38 -0500

    edited modeling and changed names

--- modeling/OMIT-core.vue
+++ modeling/OMIT-core.vue
@@ -1,18 +1,18 @@
-<!-- Tufts VUE 3.2.2 concept-map (date core.vue) 2015-05-16 -->
+<!-- Tufts VUE 3.2.2 concept-map (OMIT-core.vue) 2015-05-16 -->
 <!-- Tufts VUE: http://vue.tufts.edu/ -->
 <!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
-<!-- Do Not Remove: Saved date Sat May 16 22:46:35 CDT 2015 by yuln on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saved date Sat May 16 23:37:25 CDT 2015 by yuln on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
 <!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
 <?xml version="1.0" encoding="US-ASCII"?>
 <LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:noNamespaceSchemaLocation="none" ID="0" label="date core.vue"
+    xsi:noNamespaceSchemaLocation="none" ID="0" label="OMIT-core.vue"
     created="1431816468688" x="0.0" y="0.0" width="1.4E-45"
     height="1.4E-45" strokeWidth="0.0" autoSized="false">
-    <resource referenceCreated="1431834396056" size="24797"
-        spec="C:\Users\yuln\Documents\GitHub\ncRO\modeling\date core.vue"
+    <resource referenceCreated="1431837445997" size="28450"
+        spec="C:\Users\yuln\Documents\GitHub\ncRO\modeling\OMIT-core.vue"
         type="1" xsi:type="URLResource">
-        <title>date core.vue</title>
-        <property key="File" value="C:\Users\yuln\Documents\GitHub\ncRO\modeling\date core.vue"/>
+        <title>OMIT-core.vue</title>
+        <property key="File" value="C:\Users\yuln\Documents\GitHub\ncRO\modeling\OMIT-core.vue"/>
     </resource>
     <fillColor>#FFFFFF</fillColor>
     <strokeColor>#404040</strokeColor>
@@ -31,9 +31,8 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="7" label="miRNA and target gene assumption" layerID="1"
-        created="1431816662672" x="-34.168236" y="255.16785"
-        width="208.0" height="38.666676" strokeWidth="1.0"
-        autoSized="false" xsi:type="node">
+        created="1431816662672" x="-76.8349" y="269.8345" width="208.0"
+        height="38.666676" strokeWidth="1.0" autoSized="false" xsi:type="node">
         <fillColor>#BDE5F2</fillColor>
         <strokeColor>#000000</strokeColor>
         <textColor>#000000</textColor>
@@ -55,7 +54,7 @@
     </child>
     <child ID="9"
         label="target gene mRNA&#xa;mapped to table: geneTranscript"
-        layerID="1" created="1431817657247" x="149.99997" y="83.8329"
+        layerID="1" created="1431817657247" x="51.99997" y="65.8329"
         width="189.0" height="38.666676" strokeWidth="1.0"
         autoSized="false" xsi:type="node">
         <fillColor>#BDE5F2</fillColor>
@@ -66,7 +65,7 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="10" label="target gene" layerID="1"
-        created="1431817707782" x="177.16516" y="188.49963" width="92.0"
+        created="1431817707782" x="180.4985" y="212.49963" width="92.0"
         height="38.666676" strokeWidth="1.0" autoSized="false" xsi:type="node">
         <fillColor>#BDE5F2</fillColor>
         <strokeColor>#000000</strokeColor>
@@ -76,34 +75,34 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="11" label="is about target gene" layerID="1"
-        created="1431817734345" x="99.742905" y="225.14938" width="97.0"
-        height="30.518463" strokeWidth="1.0" autoSized="false"
+        created="1431817734345" x="89.81131" y="244.35019" width="96.0"
+        height="25.98433" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996420a147a5701bb3ae41937d3da</URIString>
-        <point1 x="114.29746" y="255.16785"/>
-        <point2 x="182.18835" y="225.64938"/>
+        <point1 x="94.38037" y="269.8345"/>
+        <point2 x="181.24223" y="244.85017"/>
         <ID1 xsi:type="node">7</ID1>
         <ID2 xsi:type="node">10</ID2>
     </child>
     <child ID="12" label="is gene template &#xa;of mRNA" layerID="1"
-        created="1431819063399" x="192.83255" y="121.99963" width="82.0"
-        height="67.0" strokeWidth="1.0" autoSized="false"
+        created="1431819063399" x="145.99924" y="103.99957" width="81.0"
+        height="109.00006" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996420a147a5701bb3ae47ab1ea47</URIString>
-        <point1 x="227.10597" y="188.49963"/>
-        <point2 x="240.55914" y="122.49963"/>
+        <point1 x="215.95325" y="212.49963"/>
+        <point2 x="157.04523" y="104.49957"/>
         <ID1 xsi:type="node">10</ID1>
         <ID2 xsi:type="node">9</ID2>
     </child>
     <child ID="13" label="ensembleGeneID" layerID="1"
-        created="1431819906617" x="38.498528" y="142.4996" width="109.0"
-        height="24.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        created="1431819906617" x="-7.5014725" y="142.4996"
+        width="109.0" height="24.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
         <fillColor>#BDE5F2</fillColor>
         <strokeColor>#000000</strokeColor>
         <textColor>#000000</textColor>
@@ -112,20 +111,20 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="14" label="obo:xref" layerID="1" created="1431819955645"
-        x="121.78601" y="165.9996" width="59.954964" height="25.1557"
+        x="74.35199" y="165.9996" width="111.522644" height="48.616104"
         strokeWidth="1.0" autoSized="false" controlCount="0"
         arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996430a147a5701bb3ae4afaa69c2</URIString>
-        <point1 x="181.24097" y="190.6553"/>
-        <point2 x="122.286" y="166.4996"/>
+        <point1 x="185.37463" y="214.1157"/>
+        <point2 x="74.85199" y="166.4996"/>
         <ID1 xsi:type="node">10</ID1>
         <ID2 xsi:type="node">13</ID2>
     </child>
     <child ID="15" label="OBI: has sepcific output" layerID="1"
-        created="1431820009816" x="466.75275" y="574.1719" width="116.0"
+        created="1431820009816" x="467.25275" y="574.1719" width="115.0"
         height="31.65625" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -170,7 +169,7 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="20" label="OBI: has sepcific output" layerID="1"
-        created="1431821080922" x="315.8562" y="447.8335" width="116.0"
+        created="1431821080922" x="316.3562" y="447.8335" width="115.0"
         height="33.5" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -183,7 +182,7 @@
         <ID2 xsi:type="node">18</ID2>
     </child>
     <child ID="21" label="miRNA" layerID="1" created="1431821124916"
-        x="-32.83483" y="190.49956" width="92.0" height="38.666676"
+        x="-102.83484" y="196.49956" width="92.0" height="38.666676"
         strokeWidth="1.0" autoSized="false" xsi:type="node">
         <fillColor>#BDE5F2</fillColor>
         <strokeColor>#000000</strokeColor>
@@ -193,15 +192,15 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="22" label="is about miRNA" layerID="1"
-        created="1431821139205" x="4.4984665" y="228.66623" width="74.0"
-        height="27.001617" strokeWidth="1.0" autoSized="false"
-        controlCount="0" arrowState="2" xsi:type="link">
+        created="1431821139205" x="-51.334885" y="234.66623"
+        width="73.0" height="35.668274" strokeWidth="1.0"
+        autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996450a147a5701bb3ae40e291c92</URIString>
-        <point1 x="52.89063" y="255.16785"/>
-        <point2 x="30.1063" y="229.16623"/>
+        <point1 x="5.0201283" y="269.8345"/>
+        <point2 x="-34.689896" y="235.16623"/>
         <ID1 xsi:type="node">7</ID1>
         <ID2 xsi:type="node">21</ID2>
     </child>
@@ -217,14 +216,14 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="24" label="miRNA target &#xa;assumption&#xa;based on"
-        layerID="1" created="1431821609430" x="-188.87798" y="276.79895"
-        width="155.71492" height="66.201355" strokeWidth="1.0"
+        layerID="1" created="1431821609430" x="-189.68747" y="277.95654"
+        width="113.35257" height="65.04376" strokeWidth="1.0"
         autoSized="false" controlCount="2" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996450a147a5701bb3ae41e240a20</URIString>
-        <point1 x="-33.663055" y="286.33676"/>
+        <point1 x="-76.8349" y="295.59738"/>
         <point2 x="-188.37798" y="342.5003"/>
         <ID1 xsi:type="node">7</ID1>
         <ID2 xsi:type="node">23</ID2>
@@ -264,7 +263,7 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="28" label="OBI: has sepcific output" layerID="1"
-        created="1431824033752" x="579.38904" y="446.5" width="116.0"
+        created="1431824033752" x="579.88904" y="446.5" width="115.0"
         height="29.5" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -287,7 +286,7 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="30" label="is about" layerID="1" created="1431827765180"
-        x="465.99817" y="286.33344" width="39.0" height="28.166565"
+        x="466.49817" y="286.33344" width="38.0" height="28.166565"
         strokeWidth="1.0" autoSized="false" controlCount="0"
         arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -310,21 +309,21 @@
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996470a147a5701bb3ae4ea603f4e</URIString>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="33" label="has subsequence" layerID="1"
-        created="1431829098917" x="268.66516" y="198.7717"
-        width="136.33331" height="14.0" strokeWidth="1.0"
+    <child ID="33" label="SO: has subsequence" layerID="1"
+        created="1431829098917" x="271.99847" y="211.2438"
+        width="132.99997" height="15.897598" strokeWidth="1.0"
         autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
         <font>Arial-plain-11</font>
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996470a147a5701bb3ae43886e9b3</URIString>
-        <point1 x="269.16516" y="206.9988"/>
-        <point2 x="404.49847" y="204.5446"/>
+        <point1 x="272.4985" y="226.6414"/>
+        <point2 x="404.49847" y="211.7438"/>
         <ID1 xsi:type="node">10</ID1>
         <ID2 xsi:type="node">17</ID2>
     </child>
-    <child ID="34" label="has subsequence" layerID="1"
-        created="1431829168835" x="439.0609" y="221.99951" width="88.0"
+    <child ID="34" label="SO: has subsequence" layerID="1"
+        created="1431829168835" x="428.5609" y="221.99951" width="109.0"
         height="30.000488" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -337,8 +336,8 @@
         <ID2 xsi:type="node">31</ID2>
     </child>
     <child ID="35" label="is quality measurement of " layerID="1"
-        created="1431830190063" x="366.39673" y="349.33337"
-        width="127.0" height="66.50009" strokeWidth="1.0"
+        created="1431830190063" x="366.89673" y="349.33337"
+        width="126.0" height="66.50009" strokeWidth="1.0"
         autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
         <textColor>#404040</textColor>
@@ -350,7 +349,7 @@
         <ID2 xsi:type="node">29</ID2>
     </child>
     <child ID="36" label="is quality measurement of " layerID="1"
-        created="1431830201685" x="497.23517" y="349.3334" width="127.0"
+        created="1431830201685" x="497.73517" y="349.3334" width="126.0"
         height="65.16675" strokeWidth="1.0" autoSized="false"
         controlCount="0" arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -399,7 +398,7 @@
         <ID2 xsi:type="node">37</ID2>
     </child>
     <child ID="41" label="is_a " layerID="1" created="1431831692947"
-        x="507.01862" y="462.3335" width="24.0" height="79.833984"
+        x="507.51862" y="462.3335" width="23.0" height="79.833984"
         strokeWidth="1.0" autoSized="false" controlCount="0"
         arrowState="2" xsi:type="link">
         <strokeColor>#404040</strokeColor>
@@ -411,17 +410,88 @@
         <ID1 xsi:type="node">6</ID1>
         <ID2 xsi:type="node">37</ID2>
     </child>
+    <child ID="42" label="GO: Molecular Function" layerID="1"
+        created="1431836872508" x="308.41324" y="44.583206"
+        width="138.0" height="45.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#BDE5F2</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747f60a147a570067924bc9512599</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="43" label="has function&#xa;(RO_0000085)" layerID="1"
+        created="1431836896826" x="306.8628" y="89.08319" width="72.0"
+        height="37.5" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747f70a147a570067924b6cb17ab3</URIString>
+        <point1 x="327.38922" y="126.08319"/>
+        <point2 x="358.33633" y="89.58319"/>
+        <ID1 xsi:type="node">44</ID1>
+        <ID2 xsi:type="node">42</ID2>
+    </child>
+    <child ID="44" label="target protein" layerID="1"
+        created="1431836905324" x="252.49716" y="126.083206"
+        width="117.0" height="38.666676" strokeWidth="1.0"
+        autoSized="false" xsi:type="node">
+        <fillColor>#BDE5F2</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747f90a147a570067924b0f52e3d6</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="45" label="GO: Biological Process" layerID="1"
+        created="1431836953531" x="484.1639" y="95.58322" width="145.0"
+        height="45.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#BDE5F2</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747fa0a147a570067924b08b1b4dd</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="46"
+        label="involved in &#xa;or regulates&#xa;(RO_0002431) "
+        layerID="1" created="1431836991910" x="368.99716" y="111.52872"
+        width="115.66675" height="42.0" strokeWidth="1.0"
+        autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747fa0a147a570067924b60768cf5</URIString>
+        <point1 x="369.49716" y="138.90773"/>
+        <point2 x="484.1639" y="126.149704"/>
+        <ID1 xsi:type="node">44</ID1>
+        <ID2 xsi:type="node">45</ID2>
+    </child>
+    <child ID="47" label="is gene template&#xa;of protein" layerID="1"
+        created="1431837226699" x="229.24786" y="164.24988" width="79.0"
+        height="48.749756" strokeWidth="1.0" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/602747fb0a147a570067924bf5a4be20</URIString>
+        <point1 x="245.4028" y="212.49963"/>
+        <point2 x="292.0929" y="164.74988"/>
+        <ID1 xsi:type="node">10</ID1>
+        <ID2 xsi:type="node">44</ID2>
+    </child>
     <layer ID="1" label="Layer 1" created="1431816468689" x="0.0"
         y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
         <URIString>http://vue.tufts.edu/rdf/resource/5ff996490a147a5701bb3ae48360eb66</URIString>
     </layer>
     <userZoom>1.5</userZoom>
-    <userOrigin x="-1442.5042" y="96.7502"/>
+    <userOrigin x="-464.25427" y="50.12481"/>
     <presentationBackground>#202020</presentationBackground>
     <PathwayList currentPathway="0" revealerIndex="-1">
         <pathway ID="0" label="Untitled Pathway" created="1431816468688"
             x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
-            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            strokeWidth="0.0" autoSized="false" currentIndex="0" open="true">
             <strokeColor>#B333CC33</strokeColor>
             <textColor>#000000</textColor>
             <font>SansSerif-plain-14</font>
@@ -469,5 +539,5 @@
     <date>2015-05-16</date>
     <modelVersion>6</modelVersion>
     <saveLocation>C:\Users\yuln\Documents\GitHub\ncRO\modeling</saveLocation>
-    <saveFile>C:\Users\yuln\Documents\GitHub\ncRO\modeling\date core.vue</saveFile>
+    <saveFile>C:\Users\yuln\Documents\GitHub\ncRO\modeling\OMIT-core.vue</saveFile>
 </LW-MAP>
